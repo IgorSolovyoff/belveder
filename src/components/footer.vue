606d72722a7e588c86e870d870675d579072ab0e
IgorSolovyoff <igorsolovyoff@ya.ru>
2018-09-27 18:38:31 +0300

    menu, footer, home page

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,7 +1,14 @@
 <template>
   <footer class="footer">
-    <p>ЗДЕСЬ БУДЕТ ПОДВАЛ</p>
-  </footer>
+    <div class="map">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3990.1545823370684!2d30.268961!3d59.997212000000005!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4696342263a273dd%3A0x57504ab9866f33d2!2z0YPQuy4g0J_QvtC70LXQstCw0Y8g0KHQsNCx0LjRgNC-0LLRgdC60LDRjywgNTTQsCwg0KHQsNC90LrRgi3Qn9C10YLQtdGA0LHRg9GA0LMsINCg0L7RgdGB0LjRjywgMTk3MTgz!5e0!3m2!1sru!2sus!4v1538061652327" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+    <div class="info">
+      <p>
+        Телефон: +7 965 757 7117 &nbsp Email: spb@stav-decor.ru
+      </p>
+    </div>
+    </footer>
 </template>
 <script>
 export default {
@@ -14,19 +21,16 @@ export default {
 p {
  color: black;
 }
-ul {
- list-style-type: none;
- padding: 0px;
- margin: 0px;
- overflow: hidden;
- text-align: center;
+footer{
+  height: 300px;
+  display: flex;
+  justify-content: space-evenly;
 }
-li {
- display: inline;
- margin: 0 10px;
 
+.info{
+flex-basis: 50%;
 }
-a {
- color: #a07038;
+.map{
+flex-basis: 50%;
 }
 </style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -2,22 +2,28 @@
 <template>
 <section class="home">
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1 id="title">Декоративные краски и штукатурки "Бельведер"</h1>
   </div>
+
 </section>
 </template>
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Здесь будет новый сайт Бельведер'
-    }
-  }
+  name: 'Home'
 }
 </script>
 <style type="text/css">
 p{
 color: #a07038;
+}
+.home{
+background-image: url(../assets/head.jpg);
+height: 705px;
+color: white;
+background-size: cover;
+width: 100%;
+}
+#title{
+padding-top: 30%;
 }
 </style>
