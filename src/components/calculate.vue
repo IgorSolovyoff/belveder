218710441ffe396bf1ea9871188f373f2bb97289
IgorSolovyoff <igorsolovyoff@ya.ru>
2018-10-07 14:41:39 +0300

    calculate component 2

--- src/components/calculate.vue
+++ src/components/calculate.vue
@@ -1,33 +1,47 @@
 <template>
   <section id="calculate">
-    <h1>Рассчитайте примерную стоимость материалов</h1>
-    <select v-model="Selected">
-      <option disabled value="">Выберите один из вариантов</option>
-      <option v-for="product in products" :title="product.title" :key="product.id">{{ product.title}} </option>
-    </select>
-    <p >Выбрано: {{ Selected }}</p>
-    <span>Введите площадь:</span>
-    <input v-model="value" placeholder="кв.м" type="number">
-    <button v-on:click="totalCost">Примерная цена материалов на кв.м:{{totalCost}}</button>
+      <p v-if="errors.length">
+        <b>Пожалуйста исправьте указанные ошибки:</b>
+      <ul>
+        <li v-for="error in errors">{{ error }}</li>
+      </ul>
+      </p>
+        <h1>Рассчитайте примерную стоимость материалов</h1>
+        <select v-model="Selected">
+          <option disabled value="">Выберите один из вариантов</option>
+          <option v-for="product in products" :title="product.title" :key="product.id" :cost="product.cost">{{ product.title}} {{product.cost}} </option>
+        </select>
+
+        <span>Введите площадь:</span>
+        <input v-model="meter" placeholder="кв.м" type="number">
+        <form @submit="checkForm" method="post">Примерная цена материалов на кв.м:</form> {{ multiply }}
+    <input type="submit" value="Отправить">
   </section>
 </template>
 
 <script>
-  import axios from "axios"
+  import axios from 'axios'
+
   export default {
     name: 'calculate',
     props: {
-      product: Object
+      product: Object,
+
     },
     data: function () {
       return{
-        Selected: '',
+        Selected: null,
         products: [],
-        value: this.value,
+        errors: [],
+        meter: null,
+        newCost: null,
+        cost: null,
+
       }
   },
     mounted() {
       this.loadData();
+      this.totalCost();
     },
     methods: {
       loadData() {
@@ -35,8 +49,23 @@
           .get('http://localhost:3000/products')
           .then(response => (this.products = response.data));
       },
-      totalCost: function () {
-        return this.price * this.value
+      totalCost() {
+        axios
+          .get('http://localhost:3000/products/')
+          .then(response => (this.cost = response.data));
+      },
+      multiply: () => cost * meter,
+      checkForm: function (e) {
+        if (this.meter) {
+          return true;
+        }
+
+        this.errors = [];
+
+        if (!this.meter) {
+          this.errors.push('Введите площадь');
+        }
+        e.preventDefault();
       }
   },
 
