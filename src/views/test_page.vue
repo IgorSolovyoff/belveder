2853e62efc04e0aa5feeebadd1bec7bc1d35b447
IgorSolovyoff <igorsolovyoff@ya.ru>
2018-10-10 16:05:33 +0300

    svg

--- src/views/test_page.vue
+++ src/views/test_page.vue
@@ -1,19 +1,31 @@
 <template>
     <section id="test_wrap">
-      <test-component></test-component>
+      <img src="@/assets/svg/paint.svg" alt="" id="paint">
     </section>
 </template>
 
 <script>
-  import testComponent from "../components/testComponent"
+
   export default {
     name: 'test_page',
     components: {
-      testComponent,
+
     }
   }
 </script>
 
 <style scoped>
-
+  section {
+    width: 100%;
+    height: 750px;
+    display: grid;
+    grid-template-columns: auto;
+    background: url(../assets/svg/bg.svg) no-repeat;
+  }
+  #paint {
+    width: 300px;
+    align-self: center;
+    justify-self: center;
+    margin-left: 500px;
+  }
 </style>
